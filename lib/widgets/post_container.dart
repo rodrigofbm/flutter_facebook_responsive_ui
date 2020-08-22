620f15a9939f8f3f73f5cdb32bca6b89c792efd0
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-08-22 15:28:57 -0300

    add post stats

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_ui/config/palette.dart';
 import 'package:facebook_ui/models/post_model.dart';
 import 'package:facebook_ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class PostContainer extends StatelessWidget {
   final Post post;
@@ -36,6 +38,10 @@ class PostContainer extends StatelessWidget {
                   child: CachedNetworkImage(imageUrl: post.imageUrl),
                 )
               : const SizedBox.shrink(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostStats(post: post),
+          ),
         ],
       ),
     );
@@ -84,6 +90,121 @@ class _PostHeader extends StatelessWidget {
               print("more");
             })
       ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+
+  const _PostStats({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                color: Palette.facebookBlue,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.thumb_up,
+                size: 10.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 4.0),
+            Expanded(
+              child: Text(
+                "${post.likes}",
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+            Text(
+              "${post.comments} comments",
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(width: 8.0),
+            Text(
+              "${post.shares} shares",
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                MdiIcons.thumbUpOutline,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: "Like",
+              onPressed: () => print("like"),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.commentOutline,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: "Comment",
+              onPressed: () => print("Comment"),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.shareOutline,
+                color: Colors.grey[600],
+                size: 25.0,
+              ),
+              label: "Share",
+              onPressed: () => print("Share"),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final Function onPressed;
+
+  const _PostButton(
+      {Key key,
+      @required this.icon,
+      @required this.label,
+      @required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: onPressed,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            height: 25.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                const SizedBox(width: 4.0),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
